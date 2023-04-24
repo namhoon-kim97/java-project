f1d537eb1edf5dbc82f4ba7dddb4a18ea1e370fa
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2023-04-24 13:32:09 +0900

    feat: Typa Casting ex

---
+++ week2/DevideIntoTwo.java
@@ -0,0 +1,14 @@
+package com.example.javaproject3.week2;
+
+public class DevideIntoTwo {
+    public static void main(String[] args) {
+        float fOne = 1;
+        float fTwo = 2;
+
+        System.out.printf("%f\n", fOne + fOne / fTwo);
+
+        int iOne = 1;
+        int iTwo = 2;
+        System.out.printf("%d\n", iOne + iOne / iTwo);
+    }
+}
