e20af89d4b70d06ea54dc59a4a63a3173f85787d
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2023-04-24 10:06:01 +0900

    feat: add DefineVariable Type

---
+++ week2/DefineVariable.java
@@ -0,0 +1,8 @@
+package com.example.javaproject3.week2;
+
+public class DefineVariable {
+    public static void main(String[] args) {
+        String name = "김남훈";
+        String name2 = new String("김미미");
+    }
+}
---
+++ week2/Type.java
@@ -0,0 +1,11 @@
+package com.example.javaproject3.week2;
+
+public class Type {
+    public static void main(String[] args) {
+        String sOne = "1";
+        int iOne = 1;
+        float fOne = 1.0f;
+
+        System.out.printf("%s %d %f", sOne, iOne, fOne);
+    }
+}
