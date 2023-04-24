45a037307e562c05e5d78b88a85e6ffce166a6b3
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2023-04-24 13:37:57 +0900

    feat: Type Casting ex(double)

---
+++ week2/DoubleNumParse.java
@@ -0,0 +1,14 @@
+package com.example.javaproject3.week2;
+
+public class DoubleNumParse {
+    public static void main(String[] args) {
+        String val1 = "2.123456789";
+        String val2 = "1.1";
+
+        double result = Double.parseDouble(val1) + Double.parseDouble(val2);
+        double result2 = Double.parseDouble(val1) + Float.parseFloat(val2);
+        System.out.println(result);
+        System.out.println(result2);
+
+    }
+}
