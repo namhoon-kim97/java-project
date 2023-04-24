73259fa3fd6d3b58f944de80673534ea7393e055
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2023-04-24 14:35:27 +0900

    feat: BooleanEx

---
+++ week2/booleanEx.java
@@ -0,0 +1,16 @@
+package com.example.javaproject3.week2;
+
+public class booleanEx {
+    public static void main(String[] args) {
+        boolean flag1 = false;
+        boolean flag2 = true;
+
+        boolean isPaymentSuccess = true;
+        boolean isBalanceSufficient = false;
+
+        System.out.println("flag1 = " + flag1);
+        System.out.println("flag2 = " + flag2);
+        System.out.println("isPaymentSuccess = " + isPaymentSuccess);
+        System.out.println("isBalanceSufficient = " + isBalanceSufficient);
+    }
+}
