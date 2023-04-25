e0869a666929a7a1dd9adb72f9bfcf95f1443728
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2023-04-25 14:29:25 +0900

    feat: Int Array Ex

---
+++ week2/day2/ArraysToString.java
@@ -0,0 +1,14 @@
+package com.example.javaproject3.week2.day2;
+
+import java.util.Arrays;
+
+public class ArraysToString {
+    public static void main(String[] args) {
+        int[] iArr = new int[5];
+        iArr[0] = 2;
+        iArr[1] = 4;
+        iArr[2] = 7;
+        iArr[3] = 8;
+        System.out.println(Arrays.toString(iArr));
+    }
+}
---
+++ week2/day2/DefAndInitArr.java
@@ -0,0 +1,8 @@
+package com.example.javaproject3.week2.day2;
+
+public class DefAndInitArr {
+    public static void main(String[] args) {
+        int[] intArr = new int[3];
+        System.out.println(intArr[0]);
+    }
+}
