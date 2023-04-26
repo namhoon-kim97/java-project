9e9ad3da1cd5e8757be82d4ec6a1356f3055bea8
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2023-04-26 11:19:35 +0900

    feat: 2D arrayIndex Ex

---
+++ week2/day3/DefineArr2.java
@@ -0,0 +1,17 @@
+package com.example.javaproject3.week2.day3;
+
+import java.util.Arrays;
+
+public class DefineArr2 {
+    public static void main(String[] args) {
+        //int[] iArr = new int[]{1,2,3,8};
+
+        int[][] arr2 = {{10,20,30}, {40,50,60}, {70,80,90}};
+        int[] arr0 = arr2[0];
+        arr0[0] = 1;
+
+        System.out.println(Arrays.toString(arr2[0]));
+        System.out.println(Arrays.toString(arr2[1]));
+        System.out.println(Arrays.toString(arr2[2]));
+    }
+}
