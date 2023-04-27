80b7bc101fadaa82475e982cfdc9389d336a4495
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2023-04-27 09:41:52 +0900

    feat: add SumOfValues
    -accumulate array values

---
+++ week2/day4/SumOfValues.java
@@ -0,0 +1,14 @@
+package com.example.javaproject3.week2.day4;
+
+public class SumOfValues {
+    public static void main(String[] args) {
+        int[] arr = {2,1,7,4};
+        int answer = 0;
+        answer += arr[0];
+        answer += arr[1];
+        answer += arr[2];
+        answer += arr[3];
+
+        System.out.println(answer);
+    }
+}
