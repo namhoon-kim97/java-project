c0dc9cee34fd04fef4e993a233dfd859b425a99b
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2023-04-27 10:42:27 +0900

    feat: Use Comparison Operators

---
+++ week2/day4/ComparisonOperatorsEx2.java
@@ -0,0 +1,15 @@
+package com.example.javaproject3.week2.day4;
+
+public class ComparisonOperatorsEx2 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.printf("%d == %d --> %b\n", a, b, a == b);
+        System.out.printf("%d != %d --> %b\n", a, b, a != b);
+        System.out.printf("%d < %d --> %b\n", a, b, a < b);
+        System.out.printf("%d <= %d --> %b\n", a, b, a <= b);
+        System.out.printf("%d > %d --> %b\n", a, b, a > b);
+        System.out.printf("%d >= %d --> %b\n", a, b, a >= b);
+
+    }
+}
