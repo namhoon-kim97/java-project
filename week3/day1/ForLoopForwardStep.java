8ccef22cb9f684b2581d0b89a30c54c1b1d646c2
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2023-05-01 13:16:42 +0900

    feat: add ForLoopForwardStep

---
+++ week3/day1/ForLoopForwardStep.java
@@ -0,0 +1,8 @@
+package com.example.javaproject3.week3.day1;
+
+public class ForLoopForwardStep {
+    public static void main(String[] args) {
+        for(int i = 1; i < 10; i += 2)
+            System.out.println(i);
+    }
+}
