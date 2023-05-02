85cbfaea2b964e5da3733ccdd8e21be447000820
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2023-05-02 14:07:58 +0900

    feat: add WhileExIncrease, WhileSleep

---
+++ week3/day2/WhileExIncrease.java
@@ -0,0 +1,22 @@
+package com.example.javaproject3.week3.day2;
+
+public class WhileExIncrease {
+    public static void main(String[] args) {
+        int cnt = 0;
+        while (cnt < 11){
+            System.out.println(cnt);
+        } // 무한루프
+
+        int cnt = 0;
+        while (cnt < 11){
+            System.out.println(cnt);
+            cnt++;
+        } // 무한루프 수정
+
+        int cnt = 0;
+        while (cnt < 11){
+            System.out.println(cnt++);
+        } // 가독성 수정
+
+    }
+}
