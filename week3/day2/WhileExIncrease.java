63355dc11aaa2d2dca79eab5430ca894d03a1a76
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2023-05-02 14:10:25 +0900

    feat: add WhileExIncrease2, WhileExIncrease수정

--- week3/day2/WhileExIncrease.java
+++ week3/day2/WhileExIncrease.java
@@ -2,7 +2,7 @@ package com.example.javaproject3.week3.day2;
 
 public class WhileExIncrease {
     public static void main(String[] args) {
-        int cnt = 0;
+        /*int cnt = 0;
         while (cnt < 11){
             System.out.println(cnt);
         } // 무한루프
@@ -16,7 +16,7 @@ public class WhileExIncrease {
         int cnt = 0;
         while (cnt < 11){
             System.out.println(cnt++);
-        } // 가독성 수정
+        } // 가독성 수정*/
 
     }
 }
