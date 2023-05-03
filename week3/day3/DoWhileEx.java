43a09f13bd126fbc47a5108f1407eabe5308b63e
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2023-05-03 10:35:53 +0900

    feat: add DoWhileEx, EvenNumberInput - do while ex

---
+++ week3/day3/DoWhileEx.java
@@ -0,0 +1,9 @@
+package com.example.javaproject3.week3.day3;
+
+public class DoWhileEx {
+    public static void main(String[] args) {
+        do{
+            System.out.println("Hello!");
+        } while(false);
+    }
+}
