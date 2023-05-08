91dc105fb1379154814c35c249fef4e8f9ba0645
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2023-05-08 13:44:34 +0900

    feat: add UserFactoryTest, UserFactory

---
+++ week4/day1/UserFactoryTest.java
@@ -0,0 +1,10 @@
+package com.example.javaproject3.week4.day1;
+
+public class UserFactoryTest {
+    public static void main(String[] args){
+        UserFactory userFactory = new UserFactory();
+        User user = userFactory.getAdultUser();
+        System.out.println(user.age);
+
+    }
+}
