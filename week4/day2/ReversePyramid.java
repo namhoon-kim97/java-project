40fcc7e3332965024d3a97584fed7b4f74360879
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2023-05-09 10:26:46 +0900

    feat: add ReversePyramid

---
+++ week4/day2/ReversePyramid.java
@@ -0,0 +1,19 @@
+package com.example.javaproject3.week4.day2;
+
+public class ReversePyramid {
+    private String spaceChar = "0";
+    public ReversePyramid(String spaceChar){
+        this.spaceChar = spaceChar;
+    }
+    public String makeALine(int h , int i){
+        return String.format("%s%s\n", spaceChar.repeat(h - i) , "*".repeat( (i * 2) - 1));
+    }
+
+    public static void main(String[] args) {
+        ReversePyramid reversePyramid = new ReversePyramid(" ");
+        int h = 5;
+        for(int i = h; i > 0; i--){
+            System.out.print(reversePyramid.makeALine(h, i));
+        }
+    }
+}
